335d5fd633b391e628c332b10a54bc84b37d53c5
masaki925 <masaki925@gmail.com>
2013-11-17 00:38:46 +0900

    RSpec

---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20131108012345) do
+
+  create_table "authentications", force: true do |t|
+    t.integer  "user_id",                   null: false
+    t.string   "provider",                  null: false
+    t.string   "uid",                       null: false
+    t.string   "access_token"
+    t.string   "permissions",  limit: 1000
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email",                                                   null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "last_login_at"
+    t.datetime "last_logout_at"
+    t.datetime "last_activity_at"
+    t.string   "last_login_from_ip_address"
+    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birthday"
+    t.string   "locale"
+    t.text     "introduction"
+    t.string   "gender"
+    t.string   "education"
+    t.string   "country_code"
+    t.string   "work"
+    t.datetime "deleted_at"
+    t.string   "image_url",                  limit: 2000
+    t.boolean  "age_is_public",                           default: true,  null: false
+    t.boolean  "official_account",                        default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
