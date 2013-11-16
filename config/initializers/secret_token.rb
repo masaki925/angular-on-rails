3719b4f5877eb8cd2c382af1953d8969a19df6f4
masaki925 <masaki925@gmail.com>
2013-11-17 00:25:34 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Compathy2::Application.config.secret_key_base = 'b7a9b2e0c1a202f7a08ad7c97d6aac6c843aa75ad813b5ff1d8a8ea72a5a9ec64f5f8b71763e1abc81faf1ec73f89b8fbf216481cd402fb2e60a3ad3bcd5de61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Compathy2::Application.config.session_store :cookie_store, key: '_compathy2_session'
